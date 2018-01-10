aca412aad3cc9b1e00f4913b34ee820ef4f26344
Deepshikha Koul <Deepshikha.Koul@proquest.com>
2018-01-09 16:21:48 -0800

    Service checkin

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,5 @@
-import {Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -10,6 +10,8 @@ export class ProductService {
   }
   getAlbum(id: number) {
     return this._http.get(this._albumUrl).map((response) => response.json());
+//
+//    return this._http.get(this._albumUrl).map(
+//      (response) => response.json());
   }
-
 }
